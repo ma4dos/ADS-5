df20dc2f38065ac96123058df72f99972e7710e5
ma4dos <dosuzhaya.mari@bk.ru>
2026-04-19 21:25:11 +0300

    Update alg.cpp
    Досужая Мария 25-ист-4-2

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,87 @@
 // Copyright 2025 NNTU-CS
-#include <string>
-#include <map>
 #include "tstack.h"
+#include <string>
+#include <sstream>
+#include <cctype>
 
-std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
-  return std::string("");
+int priority(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    return 0;
 }
 
-int eval(const std::string& pref) {
-  // добавьте код
-  return 0;
+std::string infx2pstfx(const std::string& inf) {
+    TStack<char, 100> st;
+    std::string result;
+    size_t i = 0;
+    while (i < inf.size()) {
+        char c = inf[i];
+        if (std::isspace(c)) {
+            ++i;
+            continue;
+        }
+        if (std::isdigit(c)) {
+            std::string num;
+            while (i < inf.size() && std::isdigit(inf[i])) {
+                num += inf[i];
+                ++i;
+            }
+            result += num + ' ';
+            continue;
+        }
+        if (c == '(') {
+            st.push(c);
+        } else if (c == ')') {
+            while (!st.isEmpty() && st.get() != '(') {
+                result += st.pop();
+                result += ' ';
+            }
+            if (!st.isEmpty() && st.get() == '(') {
+                st.pop();
+            }
+        } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+            while (!st.isEmpty() && st.get() != '(' && priority(st.get()) >= priority(c)) {
+                result += st.pop();
+                result += ' ';
+            }
+            st.push(c);
+        }
+        ++i;
+    }
+    while (!st.isEmpty()) {
+        result += st.pop();
+        result += ' ';
+    }
+    if (!result.empty() && result.back() == ' ') {
+        result.pop_back();
+    }
+    return result;
+}
+
+int eval(const std::string& post) {
+    std::istringstream iss(post);
+    std::string token;
+    TStack<int, 100> st;
+    while (iss >> token) {
+        if (std::isdigit(token[0]) || (token.size() > 1 && token[0] == '-' && std::isdigit(token[1]))) {
+            st.push(std::stoi(token));
+        } else {
+            int right = st.pop();
+            int left = st.pop();
+            int res;
+            if (token == "+") {
+                res = left + right;
+            } else if (token == "-") {
+                res = left - right;
+            } else if (token == "*") {
+                res = left * right;
+            } else if (token == "/") {
+                res = left / right;
+            } else {
+                res = 0;
+            }
+            st.push(res);
+        }
+    }
+    return st.pop();
 }
