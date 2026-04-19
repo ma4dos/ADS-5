d98dede6727e989db6b0d1dfdb194a50f5166028
ma4dos <dosuzhaya.mari@bk.ru>
2026-04-19 23:28:38 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,48 @@
 // Copyright 2021 NNTU-CS
+// tstack.h
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T data[size];
+    int top_;
+
+public:
+    TStack() : top_(-1) {}
+
+    void push(const T& value) {
+        if (!isFull()) {
+            data[++top_] = value;
+        }
+    }
+
+    T pop() {
+        if (!isEmpty()) {
+            return data[top_--];
+        }
+        return T();
+    }
+
+    T get() const {
+        if (!isEmpty()) {
+            return data[top_];
+        }
+        return T();
+    }
+
+    bool isEmpty() const {
+        return top_ == -1;
+    }
+
+    bool isFull() const {
+        return top_ == size - 1;
+    }
+
+    void clear() {
+        top_ = -1;
+    }
 };
 
-#endif  // INCLUDE_TSTACK_H_
+#endif
