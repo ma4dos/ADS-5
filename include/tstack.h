f95e54b0c5e4573089c31ea6122b791d9374af30
ma4dos <dosuzhaya.mari@bk.ru>
2026-04-19 22:00:59 +0300

    Update tstack.h
    Досужая ист-4-2

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,38 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template<typename T, int kSize>
 class TStack {
-  // добавьте код стека
+ private:
+  T arr_[kSize];
+  int top_;
+
+ public:
+  TStack() : top_(-1) {}
+
+  void push(T value) {
+    if (top_ < kSize - 1) {
+      arr_[++top_] = value;
+    }
+  }
+
+  T pop() {
+    if (top_ >= 0) {
+      return arr_[top_--];
+    }
+    return T();
+  }
+
+  T get() const {
+    if (top_ >= 0) {
+      return arr_[top_];
+    }
+    return T();
+  }
+
+  bool isEmpty() const {
+    return top_ == -1;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
